c8811ff576087c2ffff7a58134c53823195e1a48
Ji Yu <jyu@uchc.edu>
2025-03-04 11:20:48 -0500

    start implement tests

--- src/napari_biopb/_tests/test_widget.py
+++ src/napari_biopb/_tests/test_widget.py
@@ -1,10 +1,8 @@
-# def test_image_threshold_widget(make_napari_viewer):
-#     viewer = make_napari_viewer()
-#     layer = viewer.add_image(np.random.random((100, 100)))
-#     my_widget = BiopbImageWidget(viewer)
+import numpy as np
+from napari_biopb import BiopbImageWidget
 
-# my_widget._image_layer_combo.value = layer
-# my_widget._threshold_slider.value = 0.5
-
-# my_widget.run()
-# assert len(viewer.layers) == 2
+def test_test(make_napari_viewer):
+    viewer = make_napari_viewer()
+    layer = viewer.add_image(np.random.random((100, 100)))
+    my_widget = BiopbImageWidget(viewer)
+    assert True
